d26e0331387570eb3784b39294a958c4a5d7c6a7
David C. Larsen <dcl9934@cs.rit.edu>
2011-08-08 18:43:58 -0400

    Whoops, traces show that the base cases return None. That's not good.

--- trace.py
+++ trace.py
@@ -59,7 +59,7 @@ def foo( num = 10 ):
     """
     if num > 0:
         foo( num - 1)
-        return num
+	return num
 
 @ftrace
 def foobar( const, num = 10 ):
@@ -69,7 +69,7 @@ def foobar( const, num = 10 ):
     """
     if num > 0:
         foobar( const, num - 1 )
-        return const
+	return const
 
 @ftrace
 def one( num = 10 ):
@@ -78,15 +78,15 @@ def one( num = 10 ):
     """
     if num > 0:
         two( num - 1 )
-        return num
+	return num
 
 @ftrace
 def two( num = 10 ):
     """
     See one().
     For a good time, try removing @ftrace from either declaration and see what
-    happns.
+    happens.
     """
     if num > 0:
         one( num - 1 )
-        return num
+	return num
