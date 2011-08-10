e1b1391dea2f154b41a821bf497194c74cada192
David C. Larsen <dcl9934@cs.rit.edu>
2011-08-10 04:00:06 -0400

    Switching to a closure for ftrace fixed function-like trace definitions.
    So, now we can safely:
    
    	foo = ftrace(foo)
    
    and have our recursive calls to foo() also traced. How nice.

--- ftrace.py
+++ ftrace.py
@@ -18,26 +18,26 @@ def ftrace( function ):
     """
     Function decorator for tracing recursive calls.
 
-    You can ftrace a function by decorating it with @ftrace at defintion, as in
-    foo().
+    You can ftrace a function by decorating it with @ftrace at definition, eg:
+
+        @ftrace
+        def foo():
+            ...
+
     Note that you can also use ftrace by decorating it directly:
 
-    foo = ftrace(foo)
-
-    But recursive calls to foo will not be ftraced, because they refer to the
-    old defintion of foo. This is not a problem with the @ decoration
-    technique.
+        foo = ftrace(foo)
 
     Also, if you're using certain decorators, such as the magic @classmethod
-    and @staticmethod, you should have them exicute _after_ ftrace, like so:
+    and @staticmethod, you should have them execute _after_ ftrace, like so:
 
-    @staticmethod
-    @ftrace
-    def foo(): ...
+        @staticmethod
+        @ftrace
+        def foo(): ...
 
     or
 
-    foo = staticmethod( ftrace( foo ) )
+        foo = staticmethod( ftrace( foo ) )
 
     TODO:
     - Make pretty
@@ -50,14 +50,14 @@ def ftrace( function ):
 
     def decoration( *args, **kwargs ):
         function_string = function.__name__ + "( " + str( args ) + " " + str( kwargs ) +  " )"
-        print str(ftrace.indentation_string * ftrace.calldepth) + function_string
+        print (ftrace.indentation_string * ftrace.calldepth) + function_string
 
         # Use ftrace.calldepth to modify the variable at the class scope.
         ftrace.calldepth += 1
         retval = function( *args, **kwargs )
         ftrace.calldepth -= 1
 
-        print str(ftrace.indentation_string * ftrace.calldepth) + function_string +\
+        print (ftrace.indentation_string * ftrace.calldepth) + function_string +\
             " returns: " + str(retval)
 
         return retval
@@ -67,67 +67,3 @@ def ftrace( function ):
     decoration.__doc__ = function.__doc__
 
     return decoration
-
-@ftrace
-def foo( num = 10 ):
-    """
-    Used for testing functions with zero or one arguments, using default
-    assignment. Also great for testing values pased by keyword.
-    """
-    if num > 0:
-        foo( num - 1)
-    return num
-
-@ftrace
-def foobar( const, num = 10 ):
-    """
-    Used for testing calls that can have multiple (or singular) parameters and
-    for setting parameters by name.
-    """
-    if num > 0:
-        foobar( const, num - 1 )
-    return const
-
-@ftrace
-def one( num = 10 ):
-    """
-    Demonstrates recursive calls between two functions.
-    """
-    if num > 0:
-        two( num - 1 )
-    return num
-
-@ftrace
-def two( num = 10 ):
-    """
-    See one().
-    For a good time, try removing @ftrace from either declaration and see what
-    happens.
-    """
-    if num > 0:
-        one( num - 1 )
-    return num
-
-class test():
-    @ftrace
-    def method(self):
-        """
-        An object method.
-        """
-        return
-
-    @classmethod
-    @ftrace
-    def cls_method(cls):
-        """
-        A class method.
-        """
-        return
-
-    @staticmethod
-    @ftrace
-    def static_method():
-        """
-        A static method.
-        """
-        return
