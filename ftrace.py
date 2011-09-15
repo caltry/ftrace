f327dd5b83c2141acce88d5574d8f83cecfd6064
David C. Larsen <dcl9934@cs.rit.edu>
2011-09-15 18:32:53 -0400

    Call depth numbers and ASCII are are now easy to toggle.

--- ftrace.py
+++ ftrace.py
@@ -1,15 +1,12 @@
 # Author: David Larsen <dcl9934@cs.rit.edu>
-# Date: Mon, 08 Aug 2011 05:23:39 -0400
-# License: Public Domain
+# Date: Mon, 08 Aug 2011
+# License: BSD 2-Clause License
 
 
 """
 Function decorator for tracing recursive calls.
 
-Includes the @trace.ftrace decorator and several example functions.
-
-TODO:
-- Implement unftrace() -- could be tricky...
+Includes the @ftrace.ftrace decorator.
 """
 
 def ftrace( function ):
@@ -36,30 +33,31 @@ def ftrace( function ):
     or
 
         foo = staticmethod( ftrace( foo ) )
-
-    TODO:
-    - Make pretty
     """
 
     # Actions on variables that begin with `ftrace.' mean to do so at the class
     # scope. This allows state, such as call depth, to be persistent through
     # different function calls. See test.one() for an example.
 
-    ftrace.indentation_string = "|  "
+    # Print the number of stack frames we've traced through.
+    ftrace.PRINT_CALLDEPTH_NUM = True
+
+    # Indent functions that have a deeper call depth.
+    ftrace.PRINT_ASCII_ART = True
+
+    ftrace.INDENTATION_STRING = "|  "
     if not hasattr( ftrace, "calldepth" ):
         ftrace.calldepth = 0
 
     def decoration( *args, **kwargs ):
         function_string = function.__name__ + "( " + str( args ) + " " + str( kwargs ) +  " )"
-        print((ftrace.indentation_string * ftrace.calldepth) + function_string)
+        _print_call_depth( ftrace.calldepth, function_string )
 
-        # Use ftrace.calldepth to modify the variable at the class scope.
         ftrace.calldepth += 1
         retval = function( *args, **kwargs )
         ftrace.calldepth -= 1
 
-        print((ftrace.indentation_string * ftrace.calldepth) +\
-            function_string + " returns: " + str(retval))
+        _print_call_depth( ftrace.calldepth, function_string + " returns: " + str(retval) )
 
         return retval
 
@@ -68,3 +66,20 @@ def ftrace( function ):
     decoration.__doc__ = function.__doc__
 
     return decoration
+
+def _print_call_depth( calldepth, function_string ):
+    """
+    Generates the function string to be printed out.
+    """
+
+    calldepth_str = "{0:{width}{base}}".format(calldepth, width=5, base='d')
+
+    if ftrace.PRINT_CALLDEPTH_NUM:
+        indentation_string = calldepth_str + " "
+    else:
+        indentation_string = " "
+
+    if ftrace.PRINT_ASCII_ART:
+        indentation_string += ftrace.INDENTATION_STRING * calldepth
+
+    print( indentation_string + function_string);
